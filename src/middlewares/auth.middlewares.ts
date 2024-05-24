e8215ce10c67863dcf6146f322b7129d4b497957
alex pacheco <alexpacheco407@gmail.com>
2024-05-24 17:32:51 -0500

    se agrega validacion para intento de registro con email repetido

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -1,8 +1,9 @@
 import catchAsync from "./../utils/catchAsync";
 import AppError from "./../utils/appError";
-import User from "./../database/models/user.model";
+import User, { IUser } from "./../database/models/user.model";
 import { NextFunction, Request, Response } from "express";
 import { ExtendedRequest } from "interfaces/extended.interfaces";
+import {formatText} from "./../utils/formatText";
 
 export const validIfUserExist = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const {id} = req.params;
@@ -21,4 +22,16 @@ export const validIfUserExist = catchAsync(async (req: Request, res: Response, n
     (req as ExtendedRequest).user = user;
 
     next();
-})
+});
+
+export const validIfEmailExist = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const {email}: IUser = req.body;
+
+    const user = await User.findOne({
+        where: {
+            email: formatText(email)
+        }
+    });
+
+    if(user) return next(new AppError("The email is already registered, enter another email", 401));
+});
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {signup, signin, updateUser, updatePassword} from "../controllers/auth.controller";
 import {signupValidations, signinValidations, updateUserValidations, changePasswordValidations} from "../validations/auth.validations";
-import {validIfUserExist} from "./../middlewares/auth.middlewares";
+import {validIfUserExist, validIfEmailExist} from "./../middlewares/auth.middlewares";
 import {protect, protectedAccountOwner} from "./../middlewares/authentication.middlewares";
 
 const router = express.Router();
 
-router.post("/signup", signupValidations, signup);
+router.post("/signup", signupValidations, validIfEmailExist, signup);
 
 router.post("/signin", signinValidations, signin);
 
